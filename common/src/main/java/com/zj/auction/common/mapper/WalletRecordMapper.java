0d74d2b03ecd599f482436a59c38d7f27324602f
bageyang <1960349130@qq.com>
2024-01-05 13:54:02 +0800

    Merge remote-tracking branch 'origin/master'

--- common/src/main/java/com/zj/auction/common/mapper/WalletRecordMapper.java
+++ common/src/main/java/com/zj/auction/common/mapper/WalletRecordMapper.java
@@ -1,8 +1,10 @@
 package com.zj.auction.common.mapper;
 
 import com.zj.auction.common.model.WalletRecord;
-import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+@Mapper
 public interface WalletRecordMapper {
     int deleteByPrimaryKey(Long id);
 
--- seckill/src/main/java/com/zj/auction/seckill/service/impl/SeckillServiceImpl.java
+++ seckill/src/main/java/com/zj/auction/seckill/service/impl/SeckillServiceImpl.java
@@ -29,7 +29,12 @@ public class SeckillServiceImpl implements SeckillService {
         doCheck(auctionId);
         // 2.扣减库存 decrement
         Optional<String> ret = decreStock(auctionId);
-        return null;
+        if(!ret.isPresent()){
+            return Ret.error(StatusEnum.SECKILL_FAIL_ERROR);
+        }
+        // 3.发送mq 生成订单 todo 提前生成订单编号
+
+        return Ret.ok(ret.get());
     }
 
     private void doCheck(Long auctionId) {
@@ -52,6 +57,6 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
     private Optional<String> decreStock(Long auctionId) {
-return null;
+        return Optional.empty();
     }
 }
