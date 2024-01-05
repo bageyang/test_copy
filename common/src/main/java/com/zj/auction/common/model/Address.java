3a8d362f3eaf2446083c09ccf0904962523fa043
bageyang <1960349130@qq.com>
2024-01-05 14:32:01 +0800

    修改实体类，添加swagger注释

--- common/src/main/java/com/zj/auction/common/model/Address.java
+++ common/src/main/java/com/zj/auction/common/model/Address.java
@@ -1,305 +1,80 @@
 package com.zj.auction.common.model;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.zj.auction.common.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-
-public class Address implements Serializable {
+@Data
+@TableName("zj_address")
+public class Address extends BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "收货地址编号")
     private Long addrId;
 
     private LocalDateTime addTime;
 
-    private Long addUserId;
-
+    @ApiModelProperty(value = "省,市, 区/县")
     private String addr1;
 
+    @ApiModelProperty(value = "具体地址")
     private String addr2;
 
+    @ApiModelProperty(value = "具体地址")
     private String addr3;
 
+    @ApiModelProperty(value = "市对应的编号")
     private Integer cityId;
 
+    @ApiModelProperty(value = "区/县对应的编号")
     private Integer countyId;
 
+    @ApiModelProperty(value = "是否默认地址，0非默认，1默认")
     private Integer defaultFlag;
 
-    private Integer deleteFlag;
-
+    @ApiModelProperty(value = "收货人")
     private String name;
 
+    @ApiModelProperty(value = "邮编")
     private String postCode;
 
-    private Integer provinceId;
+    @ApiModelProperty(value = "状态")
+    private Integer status = 0;
 
-    private Integer status;
-
+    @ApiModelProperty(value = "手机号")
     private String tel1;
 
+    @ApiModelProperty(value = "固定电话")
     private String tel2;
 
+    @ApiModelProperty(value = "类型")
     private Integer typeId;
 
-    private LocalDateTime updateTime;
-
+    @ApiModelProperty(value = "用户id")
     private Long userId;
 
-    private String transactionId;
-
-    private Long updateUserId;
-
-    private String longitude;
-
-    private String latitude;
-
+    @ApiModelProperty(value = "市")
     private String city;
 
+    @ApiModelProperty(value = "区/县")
     private String county;
 
+    @ApiModelProperty(value = "省")
     private String province;
+
+    @ApiModelProperty(value = "经度")
+    private String longitude ;
+
+    @ApiModelProperty(value = "纬度")
+    private String latitude ;
 
     private static final long serialVersionUID = 1L;
 
-    public Long getAddrId() {
-        return addrId;
-    }
 
-    public void setAddrId(Long addrId) {
-        this.addrId = addrId;
-    }
-
-    public LocalDateTime getAddTime() {
-        return addTime;
-    }
-
-    public void setAddTime(LocalDateTime addTime) {
-        this.addTime = addTime;
-    }
-
-    public Long getAddUserId() {
-        return addUserId;
-    }
-
-    public void setAddUserId(Long addUserId) {
-        this.addUserId = addUserId;
-    }
-
-    public String getAddr1() {
-        return addr1;
-    }
-
-    public void setAddr1(String addr1) {
-        this.addr1 = addr1;
-    }
-
-    public String getAddr2() {
-        return addr2;
-    }
-
-    public void setAddr2(String addr2) {
-        this.addr2 = addr2;
-    }
-
-    public String getAddr3() {
-        return addr3;
-    }
-
-    public void setAddr3(String addr3) {
-        this.addr3 = addr3;
-    }
-
-    public Integer getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(Integer cityId) {
-        this.cityId = cityId;
-    }
-
-    public Integer getCountyId() {
-        return countyId;
-    }
-
-    public void setCountyId(Integer countyId) {
-        this.countyId = countyId;
-    }
-
-    public Integer getDefaultFlag() {
-        return defaultFlag;
-    }
-
-    public void setDefaultFlag(Integer defaultFlag) {
-        this.defaultFlag = defaultFlag;
-    }
-
-    public Integer getDeleteFlag() {
-        return deleteFlag;
-    }
-
-    public void setDeleteFlag(Integer deleteFlag) {
-        this.deleteFlag = deleteFlag;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPostCode() {
-        return postCode;
-    }
-
-    public void setPostCode(String postCode) {
-        this.postCode = postCode;
-    }
-
-    public Integer getProvinceId() {
-        return provinceId;
-    }
-
-    public void setProvinceId(Integer provinceId) {
-        this.provinceId = provinceId;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getTel1() {
-        return tel1;
-    }
-
-    public void setTel1(String tel1) {
-        this.tel1 = tel1;
-    }
-
-    public String getTel2() {
-        return tel2;
-    }
-
-    public void setTel2(String tel2) {
-        this.tel2 = tel2;
-    }
-
-    public Integer getTypeId() {
-        return typeId;
-    }
-
-    public void setTypeId(Integer typeId) {
-        this.typeId = typeId;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public Long getUpdateUserId() {
-        return updateUserId;
-    }
-
-    public void setUpdateUserId(Long updateUserId) {
-        this.updateUserId = updateUserId;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCounty() {
-        return county;
-    }
-
-    public void setCounty(String county) {
-        this.county = county;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", addrId=").append(addrId);
-        sb.append(", addTime=").append(addTime);
-        sb.append(", addUserId=").append(addUserId);
-        sb.append(", addr1=").append(addr1);
-        sb.append(", addr2=").append(addr2);
-        sb.append(", addr3=").append(addr3);
-        sb.append(", cityId=").append(cityId);
-        sb.append(", countyId=").append(countyId);
-        sb.append(", defaultFlag=").append(defaultFlag);
-        sb.append(", deleteFlag=").append(deleteFlag);
-        sb.append(", name=").append(name);
-        sb.append(", postCode=").append(postCode);
-        sb.append(", provinceId=").append(provinceId);
-        sb.append(", status=").append(status);
-        sb.append(", tel1=").append(tel1);
-        sb.append(", tel2=").append(tel2);
-        sb.append(", typeId=").append(typeId);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", userId=").append(userId);
-        sb.append(", transactionId=").append(transactionId);
-        sb.append(", updateUserId=").append(updateUserId);
-        sb.append(", longitude=").append(longitude);
-        sb.append(", latitude=").append(latitude);
-        sb.append(", city=").append(city);
-        sb.append(", county=").append(county);
-        sb.append(", province=").append(province);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
--- common/src/main/java/com/zj/auction/common/model/Wallet.java
+++ common/src/main/java/com/zj/auction/common/model/Wallet.java
@@ -1,11 +1,15 @@
 package com.zj.auction.common.model;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Builder;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 @Builder
+@Data
+@TableName("zj_wallet")
 public class Wallet implements Serializable {
     private Long id;
 
@@ -72,132 +76,5 @@ public class Wallet implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Integer getFundType() {
-        return fundType;
-    }
-
-    public void setFundType(Integer fundType) {
-        this.fundType = fundType;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public BigDecimal getFreeze() {
-        return freeze;
-    }
-
-    public void setFreeze(BigDecimal freeze) {
-        this.freeze = freeze;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getTransactionType() {
-        return transactionType;
-    }
-
-    public void setTransactionType(Integer transactionType) {
-        this.transactionType = transactionType;
-    }
-
-    public BigDecimal getBalanceBefore() {
-        return balanceBefore;
-    }
-
-    public void setBalanceBefore(BigDecimal balanceBefore) {
-        this.balanceBefore = balanceBefore;
-    }
-
-    public BigDecimal getUpdateBalance() {
-        return updateBalance;
-    }
-
-    public void setUpdateBalance(BigDecimal updateBalance) {
-        this.updateBalance = updateBalance;
-    }
-
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public Integer getWalletType() {
-        return walletType;
-    }
-
-    public void setWalletType(Integer walletType) {
-        this.walletType = walletType;
-    }
-
-    public String getTradeNo() {
-        return tradeNo;
-    }
-
-    public void setTradeNo(String tradeNo) {
-        this.tradeNo = tradeNo;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", userId=").append(userId);
-        sb.append(", fundType=").append(fundType);
-        sb.append(", balance=").append(balance);
-        sb.append(", freeze=").append(freeze);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", transactionType=").append(transactionType);
-        sb.append(", balanceBefore=").append(balanceBefore);
-        sb.append(", updateBalance=").append(updateBalance);
-        sb.append(", remark=").append(remark);
-        sb.append(", walletType=").append(walletType);
-        sb.append(", tradeNo=").append(tradeNo);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
