a0b396d339edeb008408f68486c6e9030e01ef5d
bageyang <1960349130@qq.com>
2024-01-05 13:57:01 +0800

    feat:

--- seckill/src/main/java/com/zj/auction/seckill/config/BaseRedisConfig.java
+++ seckill/src/main/java/com/zj/auction/seckill/config/BaseRedisConfig.java
@@ -7,8 +7,6 @@ import com.alibaba.fastjson2.support.config.FastJsonConfig;
 import com.alibaba.fastjson2.support.spring.data.redis.FastJsonRedisSerializer;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.Codec;
-import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.redisson.config.TransportMode;
